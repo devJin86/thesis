4a5807604b040295ff21eb996dca32e8ee0442bd
Jin Bok <devJin86@gmail.com>
2016-05-04 23:53:55 -0700

    Slack Oauth2

--- client/Components/HeaderNavBar.jsx
+++ client/Components/HeaderNavBar.jsx
@@ -7,34 +7,35 @@ import Avatar from 'material-ui/lib/avatar';
 import IconMenu from 'material-ui/lib/menus/icon-menu';
 import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
 import MenuItem from 'material-ui/lib/menus/menu-item';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 export default class HeaderNavBar extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    const style = {
+      margin: 12,
+    };
     return (
       <AppBar
     title="Title"
+
     iconElementLeft={
       <IconButton>
         <Avatar
           src='https://lh5.ggpht.com/0WtsR4_NKcbftR5KvnlQ9YYpkBsfglEKhsvBbe-PSOgWkuDWIw2DIUnkmbuWy7DYFQ=w300-rw'
           backgroundColor='Color.1A237E'
           />
-      </IconButton>}
+      </IconButton>
+    }
+
     iconElementRight={
-      <IconMenu
-        iconButtonElement={
-          <IconButton><MoreVertIcon /></IconButton>
-        }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-      >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help" />
-        <MenuItem primaryText="Sign out" />
-      </IconMenu>
+      <div>
+        <RaisedButton label="Primary" primary={true} style={style} />
+        <RaisedButton label="Primary" primary={true} style={style} />
+        <RaisedButton label="Primary" primary={true} style={style} />
+      </div>
     }
   />
     )
