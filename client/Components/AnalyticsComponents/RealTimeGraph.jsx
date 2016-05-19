0ea9c06c7aa5ddd7feb0013f0e3d805e01581415
Jin Bok <devJin86@gmail.com>
2016-05-18 19:35:11 -0700

    Revert "ready to push real time data visualization"
    This reverts commit e693d498c6b8a4628fbb6fc8e267802d09652785.

--- client/Components/AnalyticsComponents/RealTimeGraph.jsx
+++ client/Components/AnalyticsComponents/RealTimeGraph.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+<<<<<<< HEAD
 import d3 from '../../../node_modules/d3/d3.v4.0.0-alpha.33.min.js';
 import graph from './visualizations/RealTimeGraphD3.js';
 
+=======
+import graph from './visualizations/RealTimeGraphD3.js';
+>>>>>>> parent of e693d49... ready to push real time data visualization
 
 export default class RealTimeGraph extends React.Component {
   constructor(props) {
@@ -10,6 +14,7 @@ export default class RealTimeGraph extends React.Component {
 
   componentDidMount() {
     this.graph = graph(this.props.data);
+<<<<<<< HEAD
     window.addEventListener('resize', this.chart.resize);
   }
 
@@ -18,12 +23,25 @@ export default class RealTimeGraph extends React.Component {
     return false;
   }
 
+=======
+    // this.graph.update(this.props.data);
+    window.addEventListener('resize', this.chart.resize);
+  }
+
+
+>>>>>>> parent of e693d49... ready to push real time data visualization
   render() {
-    console.log(this.props.data);
     let style = {
+<<<<<<< HEAD
       width: '80%',
       height: '100%',
       'left': '300px',
+=======
+      width: '700px',
+      height: '500px',
+      position: 'absolute',
+      left: 300,
+>>>>>>> parent of e693d49... ready to push real time data visualization
     }
     return (
 
